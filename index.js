6042dcec9fa2cec255e986b76db2f960b839560d
Elizabeth Witengier <ewitengier@ufl.edu>
2022-07-02 16:04:59 -0400

    questions arrary

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
-// Your code here
+var questionsArr = [
+  {
+    question: 'Where was the first celebration of Independence Day held?',
+    answer: 'Philadelphia',
+    options: [
+      'Philadelphia',
+      'New York City',
+      'Washington DC',
+      'Boston',
+    ]
+  },
+  {
+    question: 'Every Independence Day, how many times does the Liberty Bell ring?',
+    answer: '13',
+    options: [
+      '13',
+      '50',
+      '25',
+      '4',
+    ]
+  },
+  {
+    question: 'What was the estimated population of the United States on July 4, 1776?',
+    answer: '2.5 million',
+    options: [
+      '2.5 million',
+      '1 million',
+      '12.5 million',
+      '125 million',
+    ]
+  },
+  {
+    question: 'Which president of the United States was the first to host a Fourth of July celebration at the White House?',
+    answer: 'Thomas Jefferson',
+    options: [
+      'Thomas Jefferson',
+      'Benjamin Franklin',
+      'George Washington',
+      'John Adams',
+    ] 
+  },
+  {
+    question: 'Every July 4th, what food is most commonly consumed?',
+    answer: 'Hot Dogs',
+    options: [
+      'Hot Dogs',
+      'BBQ Chicken',
+      'Pizza',
+      'Macaroni Salad',
+    ]
+  },
+] // Your code here
